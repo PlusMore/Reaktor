b08c041e08df0073e149224e0f4bc506bc699d5d
Patrick Scott <patrick.lee.scott@gmail.com>
2015-01-27 23:39:51 -0500

    use layout

--- neuboard-v1.1/Meteor_version/src/router.js
+++ neuboard-v1.1/Meteor_version/src/router.js
@@ -1,3 +1,9 @@
+Router.configure({
+  layoutTemplate: 'Layout'
+  // loadingTemplate: 'loading',
+  // notFoundTemplate: 'notFound'
+});
+
 Router.route('/', function () {
   this.render('Dashboard', {});
 });
