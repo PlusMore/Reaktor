69939481c293f863a2225000a92d3bbbdd4f2dc6
Patrick Scott <patrick.lee.scott@gmail.com>
2015-01-27 23:09:32 -0500

    got layout working with dashboard, adding styles now

--- neuboard-v1.1/Meteor_version/client/src/app.js
+++ neuboard-v1.1/Meteor_version/client/src/app.js
@@ -202,27 +202,32 @@ Meteor.startup(function() {
         }
 
         //DoughnutChart
-        var doughnutData = [{
+        var doughnutData = [
+            {
                 value: 300,
                 color: "#1ABC9C",
                 highlight: "#1ABC9C",
                 label: "Chrome"
-            }, {
+            }, 
+            {
                 value: 50,
                 color: "#556B8D",
                 highlight: "#556B8D",
                 label: "IE"
-            }, {
+            }, 
+            {
                 value: 100,
                 color: "#EDCE8C",
                 highlight: "#EDCE8C",
                 label: "Safari"
-            }, {
+            }, 
+            {
                 value: 40,
                 color: "#CED1D3",
                 highlight: "#1F7BB6",
                 label: "Other"
-            }, {
+            }, 
+            {
                 value: 120,
                 color: "#1F7BB6",
                 highlight: "#1F7BB6",
@@ -248,7 +253,7 @@ Meteor.startup(function() {
             });
 
         };
-    };
+
 
 
 
