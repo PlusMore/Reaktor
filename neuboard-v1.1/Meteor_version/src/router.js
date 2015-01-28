ac15c081674d0e77984adbd5bc86b13372b6485b
Patrick Scott <patrick.lee.scott@gmail.com>
2015-01-28 14:08:26 -0500

    ui elements pages

--- neuboard-v1.1/Meteor_version/src/router.js
+++ neuboard-v1.1/Meteor_version/src/router.js
@@ -22,6 +22,48 @@ Router.route('/ui-elements/sliders-and-progress', function () {
   name: 'UI.SlidersAndProgress'
 });
 
+Router.route('/ui-elements/modals-and-popups', function () {
+  this.render('ModalsAndPopups', {});
+},{
+  name: 'UI.ModalsAndPopups'
+});
+
+Router.route('/ui-elements/tabs-and-accordions', function () {
+  this.render('TabsAndAccordions', {});
+},{
+  name: 'UI.TabsAndAccordions'
+});
+
+Router.route('/ui-elements/alerts-and-notifications', function () {
+  this.render('AlertsAndNotifications', {});
+},{
+  name: 'UI.AlertsAndNotifications'
+});
+
+Router.route('/ui-elements/nestable-lists', function () {
+  this.render('NestableLists', {});
+},{
+  name: 'UI.NestableLists'
+});
+
+Router.route('/ui-elements/panels', function () {
+  this.render('Panels', {});
+},{
+  name: 'UI.Panels'
+});
+
+Router.route('/ui-elements/icons', function () {
+  this.render('Icons', {});
+},{
+  name: 'UI.Icons'
+});
+
+Router.route('/ui-elements/typography', function () {
+  this.render('Typography', {});
+},{
+  name: 'UI.Typography'
+});
+
 Router.route('/css-animations', function () {
   this.render('CSSAnimations', {});
 }, {
