418f69e4afc44d456e897dd659d03da411c6f67e
Patrick Scott <patrick.lee.scott@gmail.com>
2015-01-30 04:11:44 -0500

    fuelux and fixed dropdowns

--- neuboard-v1.1/Meteor_version/client/templates/Layout/LeftSidebar/SidebarProfile/SidebarProfile.js
+++
@@ -1,8 +0,0 @@
-Template.SidebarProfile.events({
-  'click #profile-dropdown': function (e, tmpl) {
-    e.preventDefault();
-    e.stopPropagation();
-
-    $(e.currentTarget).dropdown();
-  }
-});
