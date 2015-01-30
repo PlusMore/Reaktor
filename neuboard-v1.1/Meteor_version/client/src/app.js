99e1cf9d3ef118812d631e1f2db4a44fd7c8f62c
Patrick Scott <patrick.lee.scott@gmail.com>
2015-01-30 00:50:49 -0500

    line chart and bar chart

--- neuboard-v1.1/Meteor_version/client/src/app.js
+++ neuboard-v1.1/Meteor_version/client/src/app.js
@@ -64,113 +64,6 @@ App = function() {
         $selector.slider()
     };
 
-
-    //Chart.js LineChart, BarChart, DoughnutChart
-    var chartJs = function() {
-        //Line Charts
-        var randomScalingFactor = function() {
-            return Math.round(Math.random() * 100)
-        };
-        var lineChartData = {
-                labels: ["January", "February", "March", "April", "May", "June", "July"],
-                datasets: [{
-                    label: 'Network Usage',
-                    fillColor: 'rgba(26,188,156,0.5)',
-                    strokeColor: 'rgba(26,188,156,1)',
-                    pointColor: 'rgba(220,220,220,1)',
-                    pointStrokeColor: '#fff',
-                    pointHighlightFill: '#fff',
-                    pointHighlightStroke: 'rgba(220,220,220,1)',
-                    data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
-                }, {
-                    label: 'CPU Load',
-                    fillColor: 'rgba(31,123,182,0.5)',
-                    strokeColor: 'rgba(31,123,182,1)',
-                    pointColor: 'rgba(151,187,205,1)',
-                    pointStrokeColor: '#fff',
-                    pointHighlightFill: '#fff',
-                    pointHighlightStroke: 'rgba(151,187,205,1)',
-                    data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
-                }]
-
-            }
-            //Bar Charts
-        var randomScalingFactor = function() {
-            return Math.round(Math.random() * 100)
-        };
-        var barChartData = {
-            labels: ["January", "February", "March", "April", "May", "June", "July"],
-            datasets: [{
-                fillColor: 'rgba(26,188,156,0.5)',
-                strokeColor: 'rgba(255,255,255,0.8)',
-                highlightFill: 'rgba(26,188,156,1)',
-                highlightStroke: 'rgba(255,255,255,0.8)',
-                data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
-            }, {
-                label: 'CPU Load',
-                fillColor: 'rgba(31,123,182,0.5)',
-                strokeColor: 'rgba(255,255,255,0.8)',
-                highlightFill: 'rgba(31,123,182,1)',
-                highlightStroke: 'rgba(255,255,255,0.8)',
-                data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
-            }]
-
-        }
-
-        //DoughnutChart
-        var doughnutData = [{
-                value: 300,
-                color: "#1ABC9C",
-                highlight: "#1ABC9C",
-                label: "Chrome"
-            }, {
-                value: 50,
-                color: "#556B8D",
-                highlight: "#556B8D",
-                label: "IE"
-            }, {
-                value: 100,
-                color: "#EDCE8C",
-                highlight: "#EDCE8C",
-                label: "Safari"
-            }, {
-                value: 40,
-                color: "#CED1D3",
-                highlight: "#1F7BB6",
-                label: "Other"
-            }, {
-                value: 120,
-                color: "#1F7BB6",
-                highlight: "#1F7BB6",
-                label: "Firefox"
-            }
-
-        ];
-
-
-
-        window.onload = function() {
-            var ctx1 = document.getElementById("canvas1").getContext("2d");
-            window.myLine = new Chart(ctx1).Line(lineChartData, {
-                responsive: true
-            });
-
-            var ctx2 = document.getElementById("canvas2").getContext("2d");
-            window.myBar = new Chart(ctx2).Bar(barChartData, {
-                responsive: true
-            });
-
-            var ctx3 = document.getElementById("doughnut-chart-area").getContext("2d");
-            window.myDoughnut = new Chart(ctx3).Doughnut(doughnutData, {
-                responsive: true
-            });
-
-        };
-
-    };
-
-
-
     var nestedSortable = function() {
         var updateOutput = function(e) {
             var list = e.length ? e : $(e.target),
@@ -310,7 +203,6 @@ App = function() {
 
     //return functions
     return {
-        chartJs: chartJs,
         customCheckbox: customCheckbox,
         dateRangePicker: dateRangePicker,
         nestedSortable: nestedSortable,
@@ -322,8 +214,3 @@ App = function() {
         spinStop: spinStop
     };
 }();
-
-
-$(window).resize(function() {
-    App.chartJs();
-});
