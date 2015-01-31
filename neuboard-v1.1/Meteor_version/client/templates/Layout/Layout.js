7c087b540b7990c6450c16d529729863fb3bedb1
Patrick Scott <patrick.lee.scott@gmail.com>
2015-01-31 16:56:50 -0500

    bootstrap modals using layout region

--- neuboard-v1.1/Meteor_version/client/templates/Layout/Layout.js
+++ neuboard-v1.1/Meteor_version/client/templates/Layout/Layout.js
@@ -3,17 +3,6 @@ Session.setDefault('theme', 'theme-default');
 Template.Layout.helpers({
   theme: function () {
     return Session.get('theme');
-  },
-  modalOpen: function() {
-    // allow session variable for manual triggering of modal open
-    if (!!modal.open()) {
-      if (modal.hiding()) {
-        return 'modal-hiding';
-      }
-      return 'modal-open';
-    } else {
-      return '';
-    }
   }
 });
 
