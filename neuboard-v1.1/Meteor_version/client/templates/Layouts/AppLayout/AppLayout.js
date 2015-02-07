1b3c40525e928b62987ffa2baadbb1b343b04e23
Patrick Scott <patrick.lee.scott@gmail.com>
2015-02-07 02:03:12 -0500

    lots of refactoring and style fixes

--- neuboard-v1.1/Meteor_version/client/templates/Layouts/AppLayout/AppLayout.js
+++ neuboard-v1.1/Meteor_version/client/templates/Layouts/AppLayout/AppLayout.js
@@ -3,6 +3,9 @@ Session.setDefault('theme', 'theme-default');
 Template.AppLayout.helpers({
   theme: function () {
     return Session.get('theme');
+  },
+  ios: function() {
+    return Session.get('iOS') ? 'ios' : '';
   }
 });
 
