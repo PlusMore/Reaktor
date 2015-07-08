be7c0cf7a8aea257db59fdb4701a299ec1caac1b
Patrick Scott <patrick.lee.scott@gmail.com>
2015-07-07 23:40:46 -0400

    fix create account for not facebook

--- server/accounts.js
+++ server/accounts.js
@@ -1,7 +1,7 @@
 // See meteor accounts documentation docs.meteor.com
 
 Accounts.onCreateUser(function(options, user) {
-  if (options.profile) {
+  if (user && user.services && user.services.facebook && options.profile) {
       options.profile.pictureThumbnail = "http://graph.facebook.com/" + user.services.facebook.id + "/picture/?type=small";
       options.profile.picture = "http://graph.facebook.com/" + user.services.facebook.id + "/picture/?type=large";
       user.profile = options.profile;
