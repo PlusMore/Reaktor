19e7c0be50b65e15fad3aefc3553b4cf82180a26
Patrick Scott <patrick.lee.scott@gmail.com>
2015-07-07 22:10:29 -0400

    panel makeover, and package updates

--- client/src/hot-code-reload.js
+++ client/src/hot-code-reload.js
@@ -1,10 +1,13 @@
 // https://github.com/meteor/mobile-packages/blob/master/packages/mdg:reload-on-resume/README.md
+Meteor.startup(function() {
+  if (Meteor.isCordova) {
+    Tracker.autorun(function() {
+      var waiting = Reload.isWaitingForResume(),
+          alertId;
 
-Tracker.autorun(function() {
-  var waiting = Reload.isWaitingForResume(),
-      alertId;
-
-  if (waiting) {
-    alertId = sAlert.success('A new version is available. Restart the application to update.', {timeout: 'none'});
+      if (waiting) {
+        alertId = sAlert.success('A new version is available. Restart the application to update.', {timeout: 'none'});
+      }
+    });
   }
 });
