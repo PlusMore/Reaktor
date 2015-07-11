e257ece2e0f482b504b41479d6123301bbbcf6a0
Patrick Scott <patrick.lee.scott@gmail.com>
2015-07-11 02:19:37 -0400

    web camera styling

---
+++ client/templates/Camera/Camera.js
@@ -0,0 +1,4 @@
+Template.reaktorCamera.replaces('camera');
+Template.reaktorViewfinder.replaces('viewfinder');
+Template.reaktorCameraGenericError.replaces('genericError');
+Template.reaktorPermissionDenied.replaces('permissionDenied');
